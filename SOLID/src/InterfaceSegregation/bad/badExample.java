c50738a8231428fbc1d4d7f458d1721bbd071ce4
darrenleeleelee1 <darrenleeleelee1@gmail.com>
2020-03-29 17:52:55 +0800

    done ID

--- SOLID/src/InterfaceSegregation/bad/badExample.java
+++ SOLID/src/InterfaceSegregation/bad/badExample.java
@@ -1,4 +1,45 @@
+//不符合interface segregation
 package InterfaceSegregation.bad;
+import java.util.*;
 
 public class badExample {
+    public static void main(String[] args){
+        airplane ap = new airplane();
+        ap.action(new drone());
+        ap.action(new fighter());
+    }
+}
+class airplane{
+    public void action(tool a1){
+        a1.manned();
+        a1.fly();
+        a1.engine();
+    }
+}
+interface tool{
+    void manned();
+    void fly();
+    void engine();
+}
+class drone implements tool{
+    public void manned(){
+        System.out.println("Drone can carry people.");
+    }
+    public void fly(){
+        System.out.println("Drone can fly.");
+    }
+    public void engine(){
+        System.out.println("Drone have engine.");
+    }
+}
+class fighter implements tool{
+    public void manned(){
+        System.out.println("Fighter can carry people.");
+    }
+    public void fly(){
+        System.out.println("Fighter can fly.");
+    }
+    public void engine(){
+        System.out.println("Fighter have engine.");
+    }
 }
