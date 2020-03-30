ebfceaf18a311ab51d42655dc64e488c4b2e526f
darrenleeleelee1 <darrenleeleelee1@gmail.com>
2020-03-31 01:39:16 +0800

    modify Single Responsibility

--- SOLID/src/SingleResponsibility/bad/badExample.java
+++ SOLID/src/SingleResponsibility/bad/badExample.java
@@ -2,23 +2,16 @@ package SingleResponsibility.bad;
 
 public class badExample {
     public static void main(String[] args) {
-        Plane fighter = new Plane(1000, 9999, "SARS-CoV5", null);
-        Plane drone = new Plane(100, 0,"COVID-18", "Nikon3310");
-        //對於drone來說,他不需要bullet，bullet就是多餘的
-        //對於fighter來說，他則不需要camera
-        //修改code時就有可能發生錯誤
-        //因此我們應該把兩個class 拉到一個super class 在去實作細節
+        Plane p = new Plane();
+        p.job("Airliner");
+        p.job("Fighter");
+        //When Drone call job is weird because Drone can't carry people
+        //Not every plane can carry people, so we should distinguish every plane by thier job
+        p.job("Drone");
     }
 }
 class Plane{
-    private int gas;
-    private int bullet;
-    private String planeID;
-    private String cameraType;
-    Plane(int gas, int bullet, String name, String cameraType){
-        this.gas = gas;
-        this.bullet = bullet;
-        this.planeID = name;
-        this.cameraType = cameraType;
+    public void job(String name){
+        System.out.println(name + " can carry people.");
     }
 }
--- SOLID/src/SingleResponsibility/good/goodExample.java
+++ SOLID/src/SingleResponsibility/good/goodExample.java
@@ -2,27 +2,26 @@ package SingleResponsibility.good;
 
 public class goodExample {
     public static void main(String[] args) {
-        Drone plane1 = new Drone(100, "COVID-18", "Nikon3310");
-        Fighter plane2 = new Fighter(1000, 9999, "SARS-CoV5");
+        AirLiner ap = new AirLiner();
+        ap.job("AirLiner");
+        Drone dp = new Drone();
+        dp.job("Drone");
+        Fighter fp = new Fighter();
+        fp.job("Fighter");
+    }
+}
+class AirLiner{
+    public void job(String name){
+        System.out.println(name + " can carry people.");
     }
 }
 class Drone {
-    private int gas;
-    private String cameraType;
-    private String droneID;
-    Drone(int gas, String ID, String cameraType){
-        this.droneID = ID;
-        this.gas = gas;
-        this.cameraType = cameraType;
+    public void job(String name){
+        System.out.println(name + " can detect and take picture.");
     }
 }
 class Fighter {
-    private int gas;
-    private int bullet;
-    private String fighterID;
-    Fighter(int gas, int bullet, String name){
-        this.fighterID = name;
-        this.gas = gas;
-        this.bullet = bullet;
+    public void job(String name){
+        System.out.println(name + " can fight enemy.");
     }
 }
