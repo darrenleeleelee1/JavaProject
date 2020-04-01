4044cfa08cc5c5140bfb260ac00064e9a9a8ceaf
anne-debug <annelovedance0513@gmail.com>
2020-04-01 21:44:12 +0800

    modify badExample

--- SOLID/src/LiskovSubstitution/bad/badExample.java
+++ SOLID/src/LiskovSubstitution/bad/badExample.java
@@ -1,27 +1,62 @@
 package LiskovSubstitution.bad;
 
-public class badExample{
-    public static void main(String[] args){
-        Airplane ap = new Airplane();
-        ap.button();
-        System.out.println();
-        //當我們呼叫button時，我們有時候可能會預期是呼叫要打開逃生門，然而此時卻呼叫成自爆
-        //因此若我們能拉成一個父類別，去override他，可以讓我們更清楚呼叫這個funcion會發生什麼事
-        Drone dp = new Drone();
-        dp.button();
+import java.util.*;
+public class badExample {
 
-    }
+	public static void main(String[] args){
+
+		Airplane a = new Airplane();
+		Fighter f = new Fighter();
+		Drone d = new Drone();
+
+		System.out.println("Airplane:");
+		a.fly();
+		a.engine();
+		a.manned();
+		System.out.println();
+
+		System.out.println("Fighter:");	
+		f.fly();
+		f.engine();
+		f.manned();
+		f.fight();
+		System.out.println();
+
+		System.out.println("Drone:");
+		d.fly();
+		d.engine();
+		d.manned();
+
+	}	
 }
+
 class Airplane {
-    public void button(){
-        System.out.println("Button was pressed, \n" +
-                "the emergency exit door will open in five seconds.");
-    }
+
+	public void fly() {
+		System.out.println("can fly");
+	}
+	public void engine() {
+		System.out.println("have engine");
+	}
+	public void manned() {
+		System.out.println("can carry people");
+	}
+
 }
 
-class Drone {
-    public void button(){
-        System.out.println("Button was pressed, \n" +
-                "the drone is going to self-explosion");
-    }
+class Fighter extends Airplane {
+
+	public void fight() {
+		System.out.println("can fight");
+	}
+	
+}
+
+class Drone extends Airplane {
+
+	@Override
+	public void manned() {
+		System.out.println("can't carry people");
+	}
+
 }
